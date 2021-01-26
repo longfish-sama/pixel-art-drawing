d143baa398b11d563ddaca5e68dcd6e42aebf3ac
longfish-sama <chendangdang@live.com>
2021-01-26 22:51:31 +0800

    add new control state

--- ctrl_ram_rd.vhd
+++ ctrl_ram_rd.vhd
@@ -5,7 +5,7 @@ use IEEE.std_logic_arith.all;
 
 entity ctrl_ram_rd is
     port (
-        x_point, y_point, color_num: out std_logic_vector(7 downto 0);
+        x_point, y_point, color_num, grid_flag: out std_logic_vector(7 downto 0);
         clk_pix, rst: in std_logic;
         add_rd: out std_logic_vector(1 downto 0);
         rd_data: in std_logic_vector(7 downto 0)
@@ -15,28 +15,33 @@ end entity ctrl_ram_rd;
 architecture asm of ctrl_ram_rd is
 begin
     ram_rd: process(clk_pix, rst)
-        type state_type is (s0, s1, s2);
+        type state_type is (s0, s1, s2, s3);
         variable state: state_type;
     begin
         if rst = '0' then
             x_point<= conv_std_logic_vector(65, x_point'length);
             y_point<= conv_std_logic_vector(1, y_point'length);
             color_num<= conv_std_logic_vector(1, color_num'length);
+            grid_flag<= (others => '1');
             add_rd<= (others=> '0');
             state:= s0;
         elsif rising_edge(clk_pix) then
             case state is
                 when s0 =>
                     add_rd<= "00";
-                    x_point<= rd_data;
+                    y_point<= rd_data;
                     state:= s1;
                 when s1 =>
                     add_rd<= "01";
-                    y_point<= rd_data;
+                    color_num<= rd_data;
                     state:= s2;
                 when s2 =>
                     add_rd<= "10";
-                    color_num<= rd_data;
+                    grid_flag<= rd_data;
+                    state:= s3;
+                when s3 =>
+                    add_rd<= "11";
+                    x_point<= rd_data;
                     state:= s0;
             end case;
         end if;
--- ctrl_ram_wr.vhd
+++ ctrl_ram_wr.vhd
@@ -5,6 +5,7 @@ use IEEE.numeric_std.all;
 entity ctrl_ram_wr is
     port (
         x_point, y_point, color_num: in std_logic_vector(7 downto 0);
+        key_code: in std_logic_vector(3 downto 0);
         clk, rst: in std_logic;
         add_wr: out std_logic_vector(1 downto 0);
         ctrl_data: out std_logic_vector(7 downto 0);
@@ -13,9 +14,23 @@ entity ctrl_ram_wr is
 end entity ctrl_ram_wr;
 
 architecture asm of ctrl_ram_wr is
+    signal grid_flag: std_logic_vector(7 downto 0);
 begin
+    grid_flag_gen: process(clk, rst)
+        variable key_code_tmp: std_logic_vector(3 downto 0);
+    begin
+        if rst = '0' then
+            grid_flag<= (others => '1');
+        elsif rising_edge(clk) then
+            key_code_tmp:= key_code;
+            if key_code_tmp= "1010" then
+                grid_flag<= not grid_flag;
+            end if;
+        end if;
+    end process grid_flag_gen;
+
     ram_ctrl: process(clk, rst, x_point, y_point, color_num)
-        type state_type is (s0, s1, s2);
+        type state_type is (s0, s1, s2, s3);
         variable state: state_type;
     begin
         if rst = '0' then
@@ -38,6 +53,11 @@ begin
                 when s2 =>
                     ctrl_data<= color_num;
                     add_wr<= "10";
+                    wr_en<= '1';
+                    state:= s3;
+                when s3 =>
+                    ctrl_data<= grid_flag;
+                    add_wr<= "11";
                     wr_en<= '1';
                     state:= s0;
             end case;
